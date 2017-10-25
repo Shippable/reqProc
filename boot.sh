c55be86cdf41d71cca39918c899e738c3b2ff35f
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-10-25 15:19:49 +0530

    copies reqExec from src to dest

--- boot.sh
+++ boot.sh
@@ -3,6 +3,9 @@
 cd /home/shippable/reqProc
 mkdir -p logs
 
+REQEXEC_DEST_DIR="$REQEXEC_DIR/src"
+cp -r $REQEXEC_SRC_DIR $REQEXEC_DEST_DIR
+
 if [ "$RUN_MODE" == "dev" ]; then
   echo forever is watching file changes
   forever -w -v --minUptime 1000 --spinSleepTime 1000 app.js
