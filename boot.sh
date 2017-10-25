c4f45f235bcc8dc14816b2a7aa675aa745da0c5e
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-10-25 18:21:12 +0530

    adds comments and checks emptiness

--- boot.sh
+++ boot.sh
@@ -3,7 +3,13 @@
 cd /home/shippable/reqProc
 mkdir -p logs
 
-cp -r $REQEXEC_PATH $REQEXEC_SRC_DIR
+# REQEXEC_PATH is path to reqExec files packaged inside the reqProc docker image
+# REQEXEC_PATH is set during building docker image for reqProc
+# REQEXEC_SRC_DIR is the destination path for copying packaged reqExec content
+# REQEXEC_SRC_DIR is set during node initialization process
+if [ ! -z $REQEXEC_PATH ] && [ ! -z $REQEXEC_SRC_DIR ]; then
+  cp -r $REQEXEC_PATH $REQEXEC_SRC_DIR
+fi
 
 if [ "$RUN_MODE" == "dev" ]; then
   echo forever is watching file changes
