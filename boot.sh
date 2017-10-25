ef1fe11996d52d418ba5d07e5cb6f6bc1dda5a41
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-10-25 18:38:46 +0530

    adds explicit check for envs

--- boot.sh
+++ boot.sh
@@ -5,11 +5,19 @@ mkdir -p logs
 
 # REQEXEC_PATH is path to reqExec files packaged inside the reqProc docker image
 # REQEXEC_PATH is set during building docker image for reqProc
+if [ -z $REQEXEC_PATH ]; then
+  echo "REQEXEC_PATH env is missing"
+  exit 1
+fi
+
 # REQEXEC_SRC_DIR is the destination path for copying packaged reqExec content
 # REQEXEC_SRC_DIR is set during node initialization process
-if [ ! -z $REQEXEC_PATH ] && [ ! -z $REQEXEC_SRC_DIR ]; then
-  cp -r $REQEXEC_PATH $REQEXEC_SRC_DIR
+if [ -z $REQEXEC_SRC_DIR ]; then
+  echo "REQEXEC_SRC_DIR env is missing"
+  exit 1
 fi
+
+cp -r $REQEXEC_PATH $REQEXEC_SRC_DIR
 
 if [ "$RUN_MODE" == "dev" ]; then
   echo forever is watching file changes
