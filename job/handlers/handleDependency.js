c0874fd7eb9fa94d637430fb68b244cec4d5bba7
Bharath Upadhya <1992bharath@gmail.com>
2017-11-03 23:26:11 +0530

    adds job handlers

---
+++ job/handlers/handleDependency.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var self = handleDependency;
+module.exports = self;
+
+var pathPlaceholder = '{{TYPE}}';
+var inStepPath = './resources/' + pathPlaceholder + '/inStep.js';
+var outStepPath = './resources/' + pathPlaceholder + '/outStep.js';
+
+function handleDependency(externalBag, dependency, callback) {
+  var bag = {
+    inPayload: _.clone(externalBag.inPayload),
+    operation: externalBag.operation,
+    consoleAdapter: externalBag.consoleAdapter,
+    builderApiAdapter: externalBag.builderApiAdapter,
+    buildJobId: externalBag.buildJobId,
+    buildInDir: externalBag.buildInDir,
+    buildOutDir: externalBag.buildOutDir,
+    stepMessageFilename: externalBag.stepMessageFilename
+  };
+  bag.who = util.format('%s|job|handlers|%s', msName, self.name);
+  logger.info(bag.who, 'Inside');
+
+  async.series([
+      _checkInputParams.bind(null, bag),
+      _handleDependency.bind(null, bag, dependency)
+    ],
+    function (err) {
+      if (err)
+        logger.error(bag.who, util.format('Failed to process IN dependencies'));
+      else
+        logger.info(bag.who, 'Successfully processed IN dependencies');
+
+      return callback(err);
+    }
+  );
+
+}
+
+function _checkInputParams(bag, next) {
+  var who = bag.who + '|' + _checkInputParams.name;
+  logger.verbose(who, 'Inside');
+
+  return next();
+}
+
+function _handleDependency(bag, dependency, next) {
+  // We don't know where the group will end so need a flag
+  bag.isGrpSuccess = true;
+
+  if (dependency.operation === bag.operation.TASK) return next();
+  if (dependency.operation === bag.operation.NOTIFY) return next();
+
+  var who = bag.who + '|' + _handleDependency.name;
+  logger.verbose(who, 'Inside');
+
+  var msg = util.format('Processing %s Dependency: %s', dependency.operation,
+    dependency.name);
+  bag.consoleAdapter.openGrp(msg);
+  bag.consoleAdapter.openCmd('Dependency Info');
+  bag.consoleAdapter.publishMsg('Version Number: ' +
+    dependency.version.versionNumber);
+
+  if (dependency.version.versionName !== null)
+    bag.consoleAdapter.publishMsg('Version Name: ' +
+      dependency.version.versionName);
+  bag.consoleAdapter.closeCmd(true);
+
+  bag.consoleAdapter.openCmd('Validating ' + dependency.name + ' handler');
+
+  var dependencyHandler;
+  var dependencyHandlerPath = '';
+  var rootDir;
+  if (dependency.operation === bag.operation.IN) {
+    dependencyHandlerPath =
+      inStepPath.replace(pathPlaceholder, dependency.type);
+    rootDir = bag.buildInDir;
+  } else if (dependency.operation === bag.operation.OUT) {
+    dependencyHandlerPath =
+      outStepPath.replace(pathPlaceholder, dependency.type);
+    rootDir = bag.buildOutDir;
+  }
+  try {
+    dependencyHandler = require(dependencyHandlerPath);
+  } catch (e) {
+    logger.debug(util.inspect(e));
+  }
+
+  if (!dependencyHandler) {
+    msg = util.format('No special dependencyHandler for dependency type: %s %s',
+      dependency.operation, dependency.type);
+    bag.consoleAdapter.publishMsg(msg);
+    bag.consoleAdapter.closeCmd(true);
+    return next();
+  }
+
+  if (!rootDir) {
+    msg = util.format('No root directory for dependency type: %s %s',
+      dependency.operation, dependency.type);
+    bag.consoleAdapter.publishMsg(msg);
+    bag.isGrpSuccess = false;
+    return next(true);
+  }
+
+  // Closing the command as dependencyHandler will call it's own cmd
+  bag.consoleAdapter.publishMsg('Successfully validated handler');
+  bag.consoleAdapter.closeCmd(true);
+
+  var params = {
+    bag: bag,
+    dependency: dependency,
+    consoleAdapter: bag.consoleAdapter,
+    builderApiAdapter: bag.builderApiAdapter,
+    rawMessage: bag.rawMessage,
+    rootDir: rootDir,
+    stepMessageFilename: bag.stepMessageFilename
+  };
+
+  dependencyHandler(params,
+    function (err) {
+      if (err)
+        bag.isGrpSuccess = false;
+      return next(err);
+    }
+  );
+}
---
+++ job/handlers/resources/params/inStep.js
@@ -0,0 +1,95 @@
+'use strict';
+var self = inStep;
+module.exports = self;
+
+var fs = require('fs-extra');
+var path = require('path');
+var executeDependencyScript =
+  require('../../_common/executeDependencyScript.js');
+
+function inStep(params, callback) {
+  var bag = {
+    resBody: {},
+    dependency: params.dependency,
+    buildInDir: params.rootDir,
+    builderApiAdapter: params.builderApiAdapter,
+    consoleAdapter: params.consoleAdapter
+  };
+
+  bag.who = msName + '|_common|resources|params|' + self.name;
+  logger.verbose(bag.who, 'Starting');
+
+  bag.paramsPath =
+    path.join(bag.buildInDir, bag.dependency.name, 'params');
+
+  async.series([
+      _checkInputParams.bind(null, bag),
+      _extractParams.bind(null, bag),
+      _writeParams.bind(null, bag)
+    ],
+    function (err) {
+      logger.verbose(bag.who, 'Completed');
+      return callback(err, bag.resBody);
+    }
+  );
+}
+
+function _checkInputParams(bag, next) {
+  var who = bag.who + '|' + _checkInputParams.name;
+  logger.debug(who, 'Inside');
+
+  bag.consoleAdapter.openCmd('Validating dependencies');
+  var consoleErrors = [];
+
+  if (!bag.dependency.version.propertyBag.params)
+    consoleErrors.push(
+      util.format('%s is missing: dependency.version.propertyBag.params', who)
+    );
+
+  if (consoleErrors.length > 0) {
+    _.each(consoleErrors,
+      function (e) {
+        var msg = e;
+        logger.error(bag.who, e);
+        bag.consoleAdapter.publishMsg(msg);
+      }
+    );
+    bag.consoleAdapter.closeCmd(false);
+    return next(true);
+  }
+
+  bag.consoleAdapter.publishMsg('Successfully validated dependencies');
+  bag.consoleAdapter.closeCmd(true);
+  return next();
+}
+
+function _extractParams(bag, next) {
+  var who = bag.who + '|' + _extractParams.name;
+  logger.debug(who, 'Inside');
+
+  bag.consoleAdapter.openCmd('Extracting params');
+  bag.extractedParams = '';
+  _.each(bag.dependency.version.propertyBag.params,
+    function (paramValue, paramKey) {
+      if (paramKey === 'secure')
+        bag.extractedParams += paramValue + '\n';
+      else
+        bag.extractedParams += util.format('%s=%s\n', paramKey, paramValue);
+    }
+  );
+  bag.consoleAdapter.publishMsg('Successfully extracted params');
+  bag.consoleAdapter.closeCmd(true);
+
+  return next();
+}
+
+function _writeParams(bag, next) {
+  var who = bag.who + '|' + _writeParams.name;
+  logger.debug(who, 'Inside');
+
+  fs.outputFile(bag.paramsPath, bag.extractedParams,
+    function(err) {
+      return next(err);
+    }
+  );
+}
