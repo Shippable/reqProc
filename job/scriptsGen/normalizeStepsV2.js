0590c690e321faec214fb5858a6d99e3d6dc1fc2
Shrivara <shrivara.jois@gmail.com>
2017-11-04 23:06:44 +0530

    Normalize the runSh options

--- job/scriptsGen/normalizeStepsV2.js
+++ job/scriptsGen/normalizeStepsV2.js
@@ -3,17 +3,57 @@
 var self = normalizeStepsV2;
 module.exports = self;
 
-function normalizeStepsV2(steps) {
-  _.each(steps,
+function normalizeStepsV2(steps, defaultRuntime) {
+  var clonedSteps = _.clone(steps);
+  var defaultJobRuntime = _.clone(defaultRuntime) || {};
+  var defaultIsContainer = true;
+  var defaultContainerOpts = {
+    'imageName': 'drydock/u14pytall',
+    'imageTag': 'master',
+    'pull': true,
+    'options': '',
+    'envs': {}
+  };
+  var defaultHostOpts = {
+    'envs': {}
+  };
+
+  if (defaultJobRuntime.container === false)
+    defaultIsContainer = false;
+  if (defaultJobRuntime.container)
+    _.extend(defaultContainerOpts, defaultJobRuntime.options);
+  else
+    _.extend(defaultHostOpts, defaultJobRuntime.options);
+
+  var taskIndex = 0;
+  _.each(clonedSteps,
     function (step) {
       if (!step.TASK) return;
       if (!step.TASK.script) return;
 
-      // if script is not an array, then add it to an array
-      if (typeof step.TASK.script === 'string')
-        step.TASK.script = [step.TASK.script];
+      var task = step.TASK;
+      if (_.isString(task.script))
+        task.script = [task.script];
+
+      task.runtime = task.runtime || {};
+      if (_.isUndefined(task.runtime.container))
+        task.runtime.container = defaultIsContainer;
+
+      task.runtime.options = task.runtime.options || {};
+      if (task.runtime.container)
+        task.runtime.options =
+          _.extend(_.clone(defaultContainerOpts), task.runtime.options);
+      else
+        task.runtime.options =
+          _.extend(_.clone(defaultHostOpts), task.runtime.options);
+
+      if (_.isUndefined(task.name))
+        task.name = 'Task ' + taskIndex;
+
+      task.execOrder = taskIndex;
+      taskIndex += 1;
     }
   );
 
-  return steps;
+  return clonedSteps;
 }
