6ea5ccf250b5d0329c1dd56a286518b28640b484
Shrivara <shrivara.jois@gmail.com>
2017-11-22 12:22:16 +0530

    Manage Dockerfiles on Arch/OS basis

--- build.sh
+++ build.sh
@@ -1,41 +1,45 @@
 #!/bin/bash -e
 
-export DRYDOCK_ORG="$1"
+export ARCHITECTURE="$1"
+export OS="$2"
+export HUB_ORG="$3"
+export IMAGE_NAME="$4"
 export TAG="master"
-
-# reqProc
-export REQPROC_REPO_PATH="./IN/reqProc_repo/gitRepo"
-export IMAGE_NAME="reqproc"
-
-# ECR location
-export ECR_ORG="374168611083.dkr.ecr.us-east-1.amazonaws.com"
-export ECR_IMAGE="$ECR_ORG/$IMAGE_NAME:$TAG"
-
-# Drydock location
-export HUB_ORG="drydock"
 export HUB_IMAGE="$HUB_ORG/$IMAGE_NAME:$TAG"
+export REQPROC_REPO_PATH="./IN/reqProc_repo/gitRepo"
 
 check_input() {
-  if [ -z "$DRYDOCK_ORG" ]; then
-    echo "Missing input parameter DRYDOCK_ORG"
+  if [ -z "$ARCHITECTURE" ]; then
+    echo "Missing input parameter ARCHITECTURE"
+    exit 1
+  fi
+
+  if [ -z "$OS" ]; then
+    echo "Missing input parameter OS"
+    exit 1
+  fi
+
+  if [ -z "$HUB_ORG" ]; then
+    echo "Missing input parameter HUB_ORG"
+    exit 1
+  fi
+
+  if [ -z "$IMAGE_NAME" ]; then
+    echo "Missing input parameter IMAGE_NAME"
     exit 1
   fi
 }
 
 set_build_context() {
-  sed -i "s/{{%DRYDOCK_ORG%}}/$DRYDOCK_ORG/g" Dockerfile
-  sed -i "s/{{%TAG%}}/$TAG/g" Dockerfile
+  sed -i "s/{{%TAG%}}/$TAG/g" ./image/$ARCHITECTURE/$OS/Dockerfile
 }
 
 build_and_tag_image() {
-  docker build -t "$HUB_IMAGE" .
-  docker tag "$HUB_IMAGE" "$ECR_IMAGE"
+  docker build -f ./image/$ARCHITECTURE/$OS/Dockerfile -t "$HUB_IMAGE" .
 }
 
 push_images() {
   docker push "$HUB_IMAGE"
-  # TODO: Uncomment this whenever the ECR repository is created.
-  # docker push "$ECR_IMAGE"
 }
 
 main() {
