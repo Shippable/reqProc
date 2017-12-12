839824649a8b2d57ece77d9a75286aec626af95f
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-12-12 14:33:22 +0530

    uses ps1 extension for task yml

--- job/scriptsGen/normalizeSteps.js
+++ job/scriptsGen/normalizeSteps.js
@@ -170,7 +170,13 @@ function __generateRuntimeInfo(task, buildJobId, buildScriptsDir,
     taskName: task.name || util.format('task_%s', task.taskIndex),
     isTaskInContainer: task.runtime.container
   });
-  task.taskScriptFileName = util.format('%s_task_%s.sh', group, task.taskIndex);
+  if (global.config.shippableNodeOperatingSystem === 'WindowsServer_2016') {
+    task.taskScriptFileName = util.format('%s_task_%s.ps1', group,
+      task.taskIndex);
+  } else {
+    task.taskScriptFileName = util.format('%s_task_%s.sh', group,
+      task.taskIndex);
+  }
   if (task.runtime.container) {
     var containerName =  util.format('reqExec.%s.%s', buildJobId,
       task.taskIndex);
