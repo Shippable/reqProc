1b20a3ae9f2bc04c103764e9822d8446485133a0
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-12-13 13:33:32 +0530

    copies reqExec from container to host

--- boot.ps1
+++ boot.ps1
@@ -1,4 +1,19 @@
 #!/bin/bash -e
 
 New-Item -ItemType Directory -Force -Path .\logs
+
+# IMAGE_REQEXEC_DIR is the directory path on image.
+if (-not ($IMAGE_REQEXEC_DIR)) {
+  Write-Error "IMAGE_REQEXEC_DIR env is missing"
+  exit 1
+}
+
+# REQEXEC_DIR is the directory path on host.
+if (-not ($REQEXEC_DIR)) {
+  Write-Error "REQEXEC_DIR env is missing"
+  exit 1
+}
+
+Copy-Item $IMAGE_REQEXEC_DIR\* -Recurse -Destination $REQEXEC_DIR
+
 node app.js
