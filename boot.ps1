f66f48005f43876d6e7ad98b159f0b093bc15e4c
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-12-13 13:46:39 +0530

    fix envs

--- boot.ps1
+++ boot.ps1
@@ -3,17 +3,17 @@
 New-Item -ItemType Directory -Force -Path .\logs
 
 # IMAGE_REQEXEC_DIR is the directory path on image.
-if (-not ($IMAGE_REQEXEC_DIR)) {
+if (-not ($env:IMAGE_REQEXEC_DIR)) {
   Write-Error "IMAGE_REQEXEC_DIR env is missing"
   exit 1
 }
 
 # REQEXEC_DIR is the directory path on host.
-if (-not ($REQEXEC_DIR)) {
+if (-not ($env:REQEXEC_DIR)) {
   Write-Error "REQEXEC_DIR env is missing"
   exit 1
 }
 
-Copy-Item $IMAGE_REQEXEC_DIR\* -Recurse -Destination $REQEXEC_DIR
+Copy-Item $env:IMAGE_REQEXEC_DIR\* -Recurse -Destination $env:REQEXEC_DIR
 
 node app.js
