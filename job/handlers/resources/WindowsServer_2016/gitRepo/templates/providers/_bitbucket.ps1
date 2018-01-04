b12649b9711d16080b1082831bc3b72e445c4093
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-01-04 17:34:18 +0530

    copy contents and not dir

--- job/handlers/resources/WindowsServer_2016/gitRepo/templates/providers/_bitbucket.ps1
+++ job/handlers/resources/WindowsServer_2016/gitRepo/templates/providers/_bitbucket.ps1
@@ -112,7 +112,7 @@ Function git_sync() {
   popd
 
   echo "----> Copying to $PROJECT_CLONE_LOCATION"
-  Copy-Item $temp_clone_path -Destination $PROJECT_CLONE_LOCATION -Recurse
+  Copy-Item "$temp_clone_path\*" -Destination $PROJECT_CLONE_LOCATION -Recurse
 
   echo "----> Removing temporary data"
   Remove-Item -Recurse -Force $temp_clone_path
