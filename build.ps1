952b073d0d3988fdb19d918a88a511c622d9f9bb
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-01-16 18:43:47 +0530

    adds build.ps1 script for building windows image

---
+++ build.ps1
@@ -0,0 +1,43 @@
+$ErrorActionPreference = "Stop"
+
+$ARCHITECTURE = "$($args[0])"
+$OS = "$($args[1])"
+$HUB_ORG = "$($args[2])"
+$IMAGE_NAME = "$($args[3])"
+$TAG = "master"
+$HUB_IMAGE = "${HUB_ORG}/${IMAGE_NAME}:${TAG}"
+
+Function check_input() {
+  if (-not $ARCHITECTURE) {
+    Throw "Missing input parameter ARCHITECTURE"
+  }
+
+  if (-not $OS) {
+    Throw "Missing input parameter OS"
+  }
+
+  if (-not $HUB_ORG) {
+    Throw "Missing input parameter HUB_ORG"
+  }
+
+  if (-not $IMAGE_NAME) {
+    Throw "Missing input parameter HUB_ORG"
+  }
+}
+
+Function set_build_context() {
+  (Get-Content ./image/$ARCHITECTURE/$OS/Dockerfile) -replace '{{%TAG%}}', "$TAG" | Set-Content ./image/$ARCHITECTURE/$OS/Dockerfile
+}
+
+Function build_and_tag_image() {
+  docker build -f ./image/$ARCHITECTURE/$OS/Dockerfile -t "$HUB_IMAGE" .
+}
+
+Function push_images() {
+  docker push "$HUB_IMAGE"
+}
+
+check_input
+set_build_context
+build_and_tag_image
+push_images
