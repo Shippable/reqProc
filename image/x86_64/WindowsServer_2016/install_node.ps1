d87a9dbcff403572df33a25e1e3a088c181bbd73
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-02-01 12:55:54 +0530

    locks window-build-tools version

--- image/x86_64/WindowsServer_2016/install_node.ps1
+++ image/x86_64/WindowsServer_2016/install_node.ps1
@@ -5,7 +5,7 @@ echo "downloading node zip"
 $env:NODE_VERSION = "4.8.5"
 
 Invoke-WebRequest $('https://nodejs.org/dist/v{0}/node-v{0}-win-x64.zip' -f $env:NODE_VERSION) -OutFile 'node.zip' -UseBasicParsing ;
-echo "extracting node archive" 
+echo "extracting node archive"
 
 $sum = Expand-Archive node.zip -DestinationPath C:\ ;
   Rename-Item -Path $('C:\node-v{0}-win-x64' -f $env:NODE_VERSION) -NewName 'C:\nodejs'
@@ -18,7 +18,7 @@ New-Item $($env:APPDATA + '\npm') ;
 
 echo "installing node dependencies using npm"
 # needed to build disk-usage package for during npm install
-npm install --global --production windows-build-tools
+npm install --global --production windows-build-tools@1.3.2
 
 echo "checking node version"
 node -v
