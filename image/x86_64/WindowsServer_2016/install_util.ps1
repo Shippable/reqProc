51a651eb3945488e57c0a1d8660dae696f4f5c5d
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-02-06 16:40:09 +0530

    installs openssh and sets envs

--- image/x86_64/WindowsServer_2016/install_util.ps1
+++ image/x86_64/WindowsServer_2016/install_util.ps1
@@ -1,6 +1,6 @@
-#!/bin/bash -e
+$ErrorActionPreference = "Stop";
 
-# TODO: move this to microbase later 
+# TODO: move this to microbase later
 echo "installing chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -12,3 +12,11 @@ echo "Installing vim"
 choco install -y vim
 refreshenv
 
+echo "Installing openssh"
+choco install -y openssh
+
+echo "Installing sshd service"
+& 'C:\Program Files\OpenSSH-Win64\install-sshd.ps1'
+
+echo "Starting sshd service"
+net start sshd
